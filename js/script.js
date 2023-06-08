8bfd883f2145487eae051790bbb7bf47eb97d67d
unknown <ibrahem.youssef2020@hotmail.com>
2023-06-08 13:51:36 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const menuBtn = document.querySelector('.menu-btn'),
+    menulist = document.querySelector('.nav-container'), 
+    menuClose = document.querySelector('.x')
+
+menuBtn.addEventListener('click' , ()=> {
+    if (menulist.classList.contains('show-list')) {
+        menulist.classList.add('hide-list')
+        menulist.classList.remove('show-list')
+    }else{
+        menulist.classList.add('show-list')
+        menulist.classList.remove('hide-list')
+    }
+})
+
+menuClose.addEventListener('click' , ()=> {
+        menulist.classList.remove('show-list')
+        menulist.classList.add('hide-list')
+        console.log('vis');
+}) 
